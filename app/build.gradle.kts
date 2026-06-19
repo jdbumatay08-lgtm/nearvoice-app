1f4a2813edee5d6d1ecf8e6eaab9eedaaab72fc3
jdbumatay08-lgtm <jdbumatay08@gmail.com>
2026-06-20 05:03:14 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,7 @@
 plugins {
-    id("com.android.application") version "8.5.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.5.2"
+    id("org.jetbrains.kotlin.android") version "1.9.24"
 }
-
-// Kailangan nating i-apply ang plugins sa dulo para sa mga bagoong Gradle versions
-apply(plugin = "com.android.application")
-apply(plugin = "org.jetbrains.kotlin.android")
 
 android {
     namespace = "com.nearvoice"
@@ -35,6 +31,14 @@ android {
         jvmTarget = "17"
     }
 }
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.activity:activity-compose:1.9.2")
+    implementation(platform("androidx.compose:compose-bom:2024.09.02"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+}}
 
 dependencies {
     implementation("androidx.core:core-ktx:1.13.1")
